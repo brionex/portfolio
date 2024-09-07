876166ca652abc9cf90a72d7fbf3fb43c12f05ce
Jesus Briones <leojesusbriones2@gmail.com>
2024-09-07 18:55:39 -0500

    fix color names

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'charcoal': '#111827',
-				'rich-black': '#030712',
-				'cool-gray': '#94A3B8',
-      	'charcoal-blue': '#303C4F',
-				'sky-blue': '#32B7E8'
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#111827',
+        'black-blue': '#030712',
+        'soft-blue': '#94A3B8',
+        'steel-blue': '#303C4F',
+        'sky-blue': '#32B7E8',
+      },
+    },
+  },
+  plugins: [],
 }
