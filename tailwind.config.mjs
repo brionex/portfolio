78b0cd35762a6aee63d4a84ae4d5fa9e97ab22a2
Jesus Briones <leojesusbriones2@gmail.com>
2024-10-10 19:15:46 -0500

    configs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        'dark-blue': '#111827',
-        'black-blue': '#030712',
-        'soft-blue': '#94A3B8',
-        'steel-blue': '#303C4F',
-        'sky-blue': '#32B7E8',
+        'rich-black': '#020617',
+      },
+      fontFamily: {
+        inter: ["'Inter Variable'", 'sans-serif'],
+        menlo: ["'Menlo Variable'", 'monospace'],
       },
     },
   },
